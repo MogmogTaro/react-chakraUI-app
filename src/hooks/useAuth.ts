d4ba8a3ebad36d802431f6159f7309f303597e2e
Mayu Uchida <31529564+UCDM@users.noreply.github.com>
2022-01-07 15:23:57 +0900

    ログイン情報をcontextに保存する

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,9 +4,12 @@ import { User } from "../types/api/user";
 import { useHistory } from "react-router-dom";
 
 import { useMessage } from "./useMessage";
+
+import { useLoginUser } from "./useLoginUser";
 export const useAuth = () => {
   const history = useHistory();
 
+  const { setLoginUser } = useLoginUser();
   const { showMessage } = useMessage();
 
   const [loading, setLoading] = useState(false);
@@ -18,18 +21,20 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            setLoginUser(res.data);
             showMessage({ title: "ログインしました", status: "success" });
             history.push("/home");
           } else {
             showMessage({ title: "ユーザーが見つかりません", status: "error" });
+            setLoading(false);
           }
         })
-        .catch(() =>
-          showMessage({ title: "ログインできません", status: "error" })
-        )
-        .finally(() => setLoading(false));
+        .catch(() => {
+          showMessage({ title: "ログインできません", status: "error" });
+          setLoading(false);
+        });
     },
-    [history, showMessage]
+    [history, showMessage, setLoginUser]
   );
   return { login, loading };
 };
